621ad1f3bb24955fda1f1f9caf27addf66f01428
MMoreno2025 <mmoreno2025@cchsdons.com>
2024-10-17 11:49:22 -0700

    Create U3L7JavaClasses.java

---
+++ U3L7JavaClasses.java
@@ -0,0 +1,104 @@
+//Malia Moreno | 10 October 2024//
+package com.mycompany.u3l7javaclasses;
+
+public class U3L7JavaClasses {
+    public static void main(String[] args) {
+        System.out.println("Lets make a car!\n");
+        
+        //make class usable//
+        Car driver;
+        driver = new Car();
+        
+        //make a driver method in main to create a car type: VW Bug
+        //give them engine power and max speed
+        driver.setCarBrand("VW Bug");
+        driver.setEnginePower(174);
+        driver.setMaxSpeed(112);
+        driver.setPrice(21790);
+        System.out.println("Car: "+driver.getCarBrand()+" | Engine Power: "+driver.toString("enginepower")+" | Max Speed: "+driver.toString("maxspeed")+" | Price: "+driver.toString("price"));
+        
+        //now make car type: Toyota Prius//
+        driver.setCarBrand("Toyota Prius");
+        driver.setEnginePower(194);
+        driver.setMaxSpeed(205);
+        driver.setPrice(27950);
+        System.out.println("Car: "+driver.getCarBrand()+" | Engine Power: "+driver.toString("enginepower")+" | Max Speed: "+driver.toString("maxspeed")+" | Price: "+driver.toString("price"));
+        
+        //now make car type: BMW i8//
+        driver.setCarBrand("BMW i8");
+        driver.setEnginePower(369);
+        driver.setMaxSpeed(155);
+        driver.setPrice(163300);
+        System.out.println("Car: "+driver.getCarBrand()+" | Engine Power: "+driver.toString("enginepower")+" | Max Speed: "+driver.toString("maxspeed")+" | Price: "+driver.toString("price"));
+        
+        //now make car type: Testla Model 3//
+        driver.setCarBrand("Tesla Model 3");
+        driver.setEnginePower(510);
+        driver.setMaxSpeed(162);
+        driver.setPrice(42490);
+        System.out.println("Car: "+driver.getCarBrand()+" | Engine Power: "+driver.toString("enginepower")+" | Max Speed: "+driver.toString("maxspeed")+" | Price: "+driver.toString("price"));
+        
+        System.out.println("\n\nYes, I'm working");
+    }
+}
+
+//make a class called car that has 1 string variable = "carBrand"
+//With: int variabled "enginePower"(HP), double variable "maxSpeed"(MPH), double variabled "price"($)
+class Car{
+    private String carBrand;
+    private int enginePower;
+    private double maxSpeed, price;
+
+    //write a constructor to produce a car base//
+    public Car(){
+        carBrand = "Toyota";
+        enginePower = 160;
+        maxSpeed = 117;
+        price = 23145;
+    }
+
+    //write methods to set carBrand, enginePower, maxSPeed, and price//
+    public void setCarBrand(String input){
+        carBrand = input;
+    }
+    public void setEnginePower(int input){
+        enginePower = input;
+    }
+    public void setMaxSpeed(double input){
+        maxSpeed = input;
+    }
+    public void setPrice(double input){
+        price = input;
+    }
+
+    //write method to get carBrand, enginePower, maxSpeed, and price//
+    public String getCarBrand(){
+        return carBrand;
+    }
+    public int getEnginePower(){
+        return enginePower;
+    }
+    public double getMaxSpeed(){
+        return maxSpeed;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    //write a toString method to print out details//
+    public String toString(String input){
+        String strEngPow = Integer.toString(enginePower);
+        String strMaxSpe = Double.toString(maxSpeed);
+        String strPri = Double.toString(price);
+
+        //figure out what to return//
+        if(input.toLowerCase() == "enginepower"){
+            return strEngPow+" HP";
+        }else if(input.toLowerCase() == "maxspeed"){
+            return strMaxSpe+" MPH";
+        }else if(input.toLowerCase() == "price"){
+            return "$"+strPri;
+        }
+        return "incorrect input";
+    }
+}
